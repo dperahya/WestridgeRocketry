a12f507c452a377e25a322dd278196aa168d2c16
Hermione9834 <167822447+Hermione9834@users.noreply.github.com>
2025-09-24 15:02:28 -0700

    add LoraRecieverGPS.ino

--- mkrwanMain/LoraRecieverGPS.ino
+++ mkrwanMain/LoraRecieverGPS.ino
@@ -1,1 +1,18 @@
+#include <SPI.h>
+#include <LoRa.h>
 
+
+void setup() {
+  Serial.begin(9600);
+  LoRa.begin(915E6);  // Set frequency to 915 MHz (adjust based on region)
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    while (LoRa.available()) {
+      String received = LoRa.readString();
+      Serial.println(received);
+    }
+  }
+}
